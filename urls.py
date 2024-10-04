ed895544e4db3cc5253baadad8b5f93d9d803203
rebakkal <rebakkal40@gmail.com>
2024-10-04 12:28:29 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,7 +1,9 @@
-from django.contrib import admin
-from django.urls import include,path
+from django.urls import path
+from  . import views
+urlpatterns=[
+    path('stock/', views.stock, name='stock'),
+    path('add/', views.add, name='add'),
+    path('update/<int:id>', views.update, name='update'),
+    path('delete/<int:id>', views.delete, name='delete'),
 
-urlpatterns = [
-    path('',include('product.urls')),
-    path('admin/', admin.site.urls),
 ]
